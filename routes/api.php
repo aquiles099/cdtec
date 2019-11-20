51189d6f92e38186ab2e09f8f0bd7f8a5bc369f7
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2019-11-20 01:09:01 -0400

    Account CRU

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// accounts
+Route::get('accounts', 'AccountController@all');
+Route::get('accounts/{id}', 'AccountController@get');
+Route::post('accounts/store', 'AccountController@store');
+Route::post('accounts/update/{id}', 'AccountController@update');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
